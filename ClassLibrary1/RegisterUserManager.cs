51d31e4d43d1cc4516f99b7cb7e01540c959bb3e
fortza_stiinta_craiova <fortza_stiinta_craiova@yahoo.com>
2019-12-06 11:27:38 +0200

    de facut RegisterUserController dupa iRegister user s RegisterUserManager

--- ClassLibrary1/RegisterUserManager.cs
+++ ClassLibrary1/RegisterUserManager.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using iRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using WebApplication17.Data;
 
 namespace BusinessLayer
 {
-    class RegisterUserManager
+    class RegisterUserManager : IRegisterUser
     {
+        protected Contexts _context;
+        public RegisterUserManager(Contexts context)
+        {
+            _context = context;
+        }
+
     }
 }
--- iRepository/IRegisterUser.cs
+++ iRepository/IRegisterUser.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebApplication17.Models;
 
 namespace iRepository
 {
     public interface IRegisterUser
     {
+        RegisterUser GetAllUsers();
+        RegisterUser GetRegisterUserById(int id);
+        RegisterUser AddUser(RegisterUser registerUser);
+        RegisterUser DeleteUser(int id);
     }
 }
